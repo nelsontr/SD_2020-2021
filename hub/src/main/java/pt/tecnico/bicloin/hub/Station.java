c41e5a09011987fa7417a7f06f70942cb61dd4bb
João Lopes <swnotjoao@Air-de-Joao.lan>
2021-03-31 14:54:23 +0100

    Add Station.java + User.java

---
+++ hub/src/main/java/pt/tecnico/bicloin/hub/Station.java
@@ -0,0 +1,44 @@
+package pt.tecnico.bicloin.hub;
+
+public class Station {
+  private String _name;
+  private String _id;
+  private float _lat;
+  private float _long;
+  private int _dockCapacity;
+  private int _prize;
+
+
+  public Station(String name, String id, float lat, float lg, int dockCapacity, int prize){
+    _name = name;
+    _id = id;
+    _lat = lat;
+    _long = lg;
+    _dockCapacity = dockCapacity;
+    _prize = prize;
+  }
+
+  public String getName(){
+    return this._name;
+  }
+
+  public String getId(){
+    return this._id;
+  }
+
+  public int getLat(){
+    return this._lat;
+  }
+
+  public int getLong(){
+    return this._long;
+  }
+
+  public int getDockCapacity(){
+    return this._dockCapacity;
+  }
+
+  public int getPrize(){
+    return this._prize;
+  }
+}
---
+++ hub/src/main/java/pt/tecnico/bicloin/hub/User.java
@@ -0,0 +1,25 @@
+package pt.tecnico.bicloin.hub;
+
+public class User {
+  private String _id;
+  private String _name;
+  private int _phoneNumber;
+
+  public User(String id, String name, int phoneNumber){
+    _id = id;
+    _name = name;
+    _phoneNumber = phoneNumber;
+  }
+
+  public String getId(){
+    return this._id;
+  }
+
+  public String getName(){
+    return this._name;
+  }
+
+  public int getPhoneNumber(){
+    return this._phoneNumber;
+  }
+}

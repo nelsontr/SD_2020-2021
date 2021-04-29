475d7c73544c32472adf2f9ed46238ff18b24b37
Nelson Trindade <nelson.trindade@tecnico.ulisboa.pt>
2021-04-29 22:07:50 +0100

    fixing cvs problems

--- hub/src/main/java/pt/tecnico/bicloin/hub/HubMain.java
+++ hub/src/main/java/pt/tecnico/bicloin/hub/HubMain.java
@@ -7,6 +7,7 @@ import io.grpc.ServerBuilder;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 import pt.tecnico.bicloin.hub.grpc.*;
@@ -63,24 +64,21 @@ public class HubMain {
 
             // Init with users + stations
             //  Users
+            Scanner fileScanner;
             String initialData = "";
-            try (Scanner fileScanner = new Scanner(new File(users))) {
-                while (fileScanner.hasNextLine()) {
-                    initialData = initialData.concat(fileScanner.nextLine() + "\n");
-                }
-            } catch (FileNotFoundException fife) {
-                System.out.println(String.format("Could not find file '%s'", users));
-                throw fife;
-            }
 
-            //  Stations
-            try (Scanner fileScanner = new Scanner(new File(stations))) {
+            if (!users.isBlank()) {
+                fileScanner = new Scanner(Objects.requireNonNull(HubMain.class.getResourceAsStream("/" + users)));
                 while (fileScanner.hasNextLine()) {
                     initialData = initialData.concat(fileScanner.nextLine() + "\n");
                 }
-            } catch (FileNotFoundException fife) {
-                System.out.println(String.format("Could not find file '%s'", stations));
-                throw fife;
+            }
+            //  Stations
+            if (!stations.isBlank()) {
+                fileScanner = new Scanner(Objects.requireNonNull(HubMain.class.getResourceAsStream("/" + stations)));
+                while (fileScanner.hasNextLine()) {
+                    initialData = initialData.concat(fileScanner.nextLine() + "\n");
+                }
             }
 
             impl.initData(initialData, initOption);

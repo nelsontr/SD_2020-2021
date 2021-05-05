287f68f7baf4a8c5223326922df7fa5da3a73c92
ist178631 <maria.sbrancia@tecnico.ulisboa.pt>
2021-05-05 02:50:37 +0100

    Read and Write algorithm

--- rec/src/main/java/pt/tecnico/rec/Record.java
+++ rec/src/main/java/pt/tecnico/rec/Record.java
@@ -1,12 +1,18 @@
 package pt.tecnico.rec;
 
 public class Record {
-    String _name;
-    int _value;
+    private String _name;
+    private int _value;
+    private int _sequence;
+    private int _cid;
 
-    public Record(String name, int value) {
-        _name = name;
-        _value = value;
+
+    public Record(String name, int value, int sequence, int cid) {
+        this._name = name;
+        this._value = value;
+        this._sequence = sequence;
+        this._cid = cid;
+
     }
 
     public String getName() {
@@ -17,6 +23,14 @@ public class Record {
         return _value;
     }
 
+    public int getSequence() {
+        return _sequence;
+    }
+
+    public int getCid() {
+        return _cid;
+    }
+
     public void setName(String name) {
         _name = name;
     }
@@ -24,4 +38,14 @@ public class Record {
     public void setValue(int value) {
         _value = value;
     }
+
+    public void setSequence(int sequence) {
+        _sequence = sequence;
+    }
+
+    public void setCid(int cid) {
+        _cid = cid;
+    }
+
+    
 }
--- rec/src/main/java/pt/tecnico/rec/RecordServiceImpl.java
+++ rec/src/main/java/pt/tecnico/rec/RecordServiceImpl.java
@@ -10,11 +10,11 @@ import static io.grpc.Status.INVALID_ARGUMENT;
 
 public class RecordServiceImpl extends RecordGrpc.RecordImplBase {
 
-    private Map<String, Integer> _records = new HashMap<>();
+    private final Map<String, Record> _records = new HashMap<>();
 
     private final String OK_RESPONSE = "OK";
     private final String ERROR_RESPONSE = "ERROR";
-    private final String NO_INPUT_FOUND = "No {} was found!";
+    private final String READ_ERROR_RESPONSE = "Read operation requires a Rec name";
     private final String REQUEST_EMPTY = "Request cannot be empty!";
     private final String INTEGER_BELOW_ZERO = "Request came with a inputValue lower than zero!";
 
@@ -23,7 +23,10 @@ public class RecordServiceImpl extends RecordGrpc.RecordImplBase {
         String input = request.getInput();
 
         if (input == null || input.isBlank()) {
-            responseObserver.onError(INVALID_ARGUMENT.withDescription(REQUEST_EMPTY).asRuntimeException());
+            synchronized(responseObserver) {
+                responseObserver.onError(INVALID_ARGUMENT.withDescription(REQUEST_EMPTY).asRuntimeException());
+            }
+            return;
         }
 
         String output = "Hello " + input + "!";
@@ -34,74 +37,93 @@ public class RecordServiceImpl extends RecordGrpc.RecordImplBase {
     }
 
     @Override
-    public void read(ReadRequest request, StreamObserver<ReadResponse> observerResponse) {
+    public void read(ReadRequest request, StreamObserver<ReadResponse> responseObserver) {
 
         String input = request.getName();
-
-        ReadResponse response;
-        
-        synchronized(this) {
-            if (input.isBlank()) {
-                observerResponse.onError(INVALID_ARGUMENT.withDescription(REQUEST_EMPTY).asRuntimeException());
-            } else if (!_records.containsKey(input)) {
-                response = ReadResponse.newBuilder().setValue(-1).build();
-                observerResponse.onNext(response);
-                observerResponse.onCompleted();
-            } else {
-                int output = _records.get(input);
-                response = ReadResponse.newBuilder().setValue(output).build();
-                observerResponse.onNext(response);
-                observerResponse.onCompleted();
-            }
-        }
-        
-    }
-
-    @Override
-    public void write(WriteRequest request, StreamObserver<WriteResponse> observerResponse) {
-
-        String output;
-        String input = request.getName();
-        Integer inputValue = request.getIntValue();
 
         if (input == null || input.isBlank()) {
-            observerResponse.onError(INVALID_ARGUMENT.withDescription(REQUEST_EMPTY).asRuntimeException());
-            return;
-        } else if (inputValue < 0) {
-            observerResponse.onError(INVALID_ARGUMENT.withDescription(INTEGER_BELOW_ZERO).asRuntimeException());
+            synchronized (responseObserver) {
+                responseObserver.onError(INVALID_ARGUMENT.withDescription(READ_ERROR_RESPONSE).asRuntimeException());
+            }
             return;
         }
 
+        synchronized(this) {
+            Record readRecord = _records.get(input);
+            if(readRecord == null) {
+                readRecord = new Record(input, -1, 0, 0);
+                _records.put(input, readRecord);
+            }
+
+            ReadResponse response = ReadResponse.newBuilder().setValue(readRecord.getValue())
+                    .setSequence(readRecord.getSequence()).setCid(readRecord.getCid()).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void write(WriteRequest request, StreamObserver<WriteResponse> responseObserver) {
+
+        final String input = request.getName();
+        final int inputValue = request.getIntValue();
+
+        if (input == null || input.isBlank()) {
+            synchronized (responseObserver) {
+                responseObserver.onError(INVALID_ARGUMENT.withDescription(REQUEST_EMPTY).asRuntimeException());
+            }
+            return;
+        } else if (inputValue < 0) {
+            synchronized (responseObserver) {
+                responseObserver.onError(INVALID_ARGUMENT.withDescription(INTEGER_BELOW_ZERO).asRuntimeException());
+            }
+            return;
+        }
+
+        final int inputSequente = request.getSequence();
+        final int inputCid = request.getCid();
+
         synchronized(this){
-            _records.put(input, inputValue);
 
-            WriteResponse response = WriteResponse.newBuilder().setResponse(OK_RESPONSE).build();
+            Record readRecord = _records.get(input);
+            
+            if(readRecord == null) {
+                readRecord = new Record(input, inputValue, inputSequente, inputCid);
+                _records.put(input, readRecord);
+            } else if (inputSequente > readRecord.getSequence() || (inputSequente == readRecord.getSequence() && inputCid > readRecord.getCid())) {
+                readRecord.setValue(inputValue);
+                readRecord.setSequence(inputSequente);
+                readRecord.setCid(inputCid);
+                _records.put(readRecord.getName(), readRecord);
+            }
 
-            observerResponse.onNext(response);
-            observerResponse.onCompleted();
+            final WriteResponse response = WriteResponse.newBuilder().setResponse(OK_RESPONSE).build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
         }
         
     }
 
     @Override
-    public void clearRecords(ClearRequest request, StreamObserver<ClearResponse> observerResponse) {
+    public void clearRecords(ClearRequest request, StreamObserver<ClearResponse> responseObserver) {
         synchronized(this){
              _records.clear();
 
             ClearResponse response = ClearResponse.newBuilder().build();
 
-            observerResponse.onNext(response);
-            observerResponse.onCompleted();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
         }
        
     }
 
     @Override
-    public void sysStat(SysStatRequest request, StreamObserver<SysStatResponse> observerResponse) {
+    public void sysStat(SysStatRequest request, StreamObserver<SysStatResponse> responseObserver) {
 
         SysStatResponse response = SysStatResponse.newBuilder().setStatus("UP").build();
 
-        observerResponse.onNext(response);
-        observerResponse.onCompleted();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
